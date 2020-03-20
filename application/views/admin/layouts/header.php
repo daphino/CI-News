1646b2bbd059bf607a7a14bf310e9a80f71e4018
Davi Nomoeh Dani <53916093+daphino@users.noreply.github.com>
2020-03-21 02:10:25 +0700

    Changin admin panel color

--- application/views/admin/layouts/header.php
+++ application/views/admin/layouts/header.php
@@ -23,6 +23,12 @@
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="<?= base_url('assets/js/dropzone.min.js') ?>"></script>
 
+	<style>
+		.btn-primary {
+			background: #179F31 !important;
+		}
+	</style>
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
--- application/views/admin/layouts/sidebar.php
+++ application/views/admin/layouts/sidebar.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #179F31 !important;">
     <!-- Brand Logo -->
     <a href="javascript:void(0)" class="brand-link">
       <span class="brand-text font-weight-light">Operator</span>
@@ -8,7 +8,7 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex" style="border-bottom-color: #179F31 !important;">
         <div class="image">
           <img src="<?= base_url('assets/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
         </div>
